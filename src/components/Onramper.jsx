d6d912c2e553c4307d19408f12a27a9c3cac4dbf
Nuno Gonçalves <nunogoncalves@mercurylabs.xyz>
2022-09-12 14:33:31 +0100

    Update Onramper.jsx
    Signed-off-by: Nuno Gonçalves <nunogoncalves@mercurylabs.xyz>

--- src/components/Onramper.jsx
+++ src/components/Onramper.jsx
@@ -1,12 +1,12 @@
 function Onramper() {
     return (
         <iframe
-            tyle="--border-radius: 10px; box-shadow: 0 2px 10px 0 rgba(0,0,0,.20); border-radius: var(--border-radius); margin: auto;max-width: 420px"
+            style="--border-radius: 10px; box-shadow: 0 2px 10px 0 rgba(0,0,0,.20); border-radius: var(--border-radius); margin: auto;max-width: 420px"
             src="https://widget.onramper.com?color=266677&apiKey=pk_test_RfO_G0PdwKduZb6yWataPBMbTyKItJ6DTvEpzrQfuWk0"
             height="660px"
             width="482px"
-            title="Onramper widget"
-            frameBorder="0"
+            title="Onramper"
+            frameborder="0"
             allow="accelerometer; autoplay; camera; gyroscope; payment"
         ></iframe>
     );
