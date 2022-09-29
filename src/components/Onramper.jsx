8fc88de16c0319e5049d1722bbb1d85043397d54
Michael <michaelksullivan50@gmail.com>
2022-09-29 10:55:44 +0200

    Fix on-ramper bug
    Needed to specify style on iframe as an object

--- src/components/Onramper.jsx
+++ src/components/Onramper.jsx
@@ -1,7 +1,7 @@
 function Onramper() {
     return (
         <iframe
-            style="--border-radius: 10px; box-shadow: 0 2px 10px 0 rgba(0,0,0,.20); border-radius: var(--border-radius); margin: auto;max-width: 420px"
+            style={{ borderRadius: "10px", boxShadow: "0 2px 10px 0 rgba(0,0,0,.20)", margin: "auto", maxWidth: "420px" }}
             src="https://widget.onramper.com?color=266677&apiKey=pk_test_X1pZ0ITJMHaxvZKyDICEXDKoX08XuhYqQtzONKDaeTo0"
             height="660px"
             width="482px"
