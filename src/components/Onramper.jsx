250a8a18358f3f22db2c0fc2a85eeca626dfa05e
Michael <michaelksullivan50@gmail.com>
2022-10-04 11:07:04 +0200

    Update to react-scripts 5 to fix compilation errors
    Updated to react-scripts 5
    Created override file with webpack and using react-app-rewired

--- src/components/Onramper.jsx
+++ src/components/Onramper.jsx
@@ -1,14 +1,14 @@
 function Onramper() {
     return (
-        <iframe
-            style={{ borderRadius: "10px", boxShadow: "0 2px 10px 0 rgba(0,0,0,.20)", margin: "auto", maxWidth: "420px" }}
-            src="https://widget.onramper.com?color=266677&apiKey=pk_test_X1pZ0ITJMHaxvZKyDICEXDKoX08XuhYqQtzONKDaeTo0"
-            height="660px"
-            width="482px"
-            title="Onramper"
-            frameBorder="0"
-            allow="accelerometer; autoplay; camera; gyroscope; payment"
-        ></iframe>
+            <iframe
+                style={{ borderRadius: "10px", boxShadow: "0 2px 10px 0 rgba(0,0,0,.20)", margin: "auto", maxWidth: "420px" }}
+                src="https://widget.onramper.com?color=266677&apiKey=pk_test_X1pZ0ITJMHaxvZKyDICEXDKoX08XuhYqQtzONKDaeTo0"
+                height="660px"
+                width="482px"
+                title="Onramper"
+                frameBorder="0"
+                allow="accelerometer; autoplay; camera; gyroscope; payment"
+            ></iframe>
     );
 }
 
