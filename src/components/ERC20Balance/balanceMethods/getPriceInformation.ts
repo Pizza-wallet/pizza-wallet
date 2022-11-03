feddfdd45aed8cdaaae8c4739f350ff4da1a602e
Michael <michaelksullivan50@gmail.com>
2022-11-03 10:11:31 +0100

    Convert files to typescript

--- src/components/ERC20Balance/balanceMethods/getPriceInformation.ts
+++ src/components/ERC20Balance/balanceMethods/getPriceInformation.ts
@@ -4,7 +4,35 @@ import {
 } from "../../../services/PriceService";
 import { getChainDetails } from "../../../helpers/getChainDetails";
 
-const updateTokensWithPriceInfo = (balances, priceInfo) => {
+interface IBalance {
+  address: string;
+  amount: string;
+  blockNumber: number;
+  chainId: number;
+  decimals: number;
+  logoURI: string;
+  name: string;
+  symbol: string;
+}
+
+interface IChainsWithId {
+  chain_identifier?: string;
+  id: string;
+  name: string;
+  shortname: string;
+}
+
+interface IPrice {
+  usd: number;
+  eur: number;
+  gbp: number;
+}
+type IPriceInfo = Record<string, IPrice>;
+
+const updateTokensWithPriceInfo = (
+  balances: IBalance[],
+  priceInfo: IPriceInfo,
+) => {
   return balances.map((token) => {
     const price = priceInfo[token.address.toLowerCase()].usd;
     return {
@@ -13,20 +41,23 @@ const updateTokensWithPriceInfo = (balances, priceInfo) => {
       value: Number(token.amount) * price,
       prices: priceInfo[token.address],
       balance: Number(token.amount),
-      chainLogoUri: getChainDetails(token.chainId).logoUri,
+      chainLogoUri: getChainDetails(token.chainId)?.logoUri,
       type: "token",
     };
   });
 };
 
-export const getPriceInformation = async (balances, chainId) => {
-  let chainsWithId = await getCoinGeckoChains();
+export const getPriceInformation = async (
+  balances: IBalance[],
+  chainId: string,
+) => {
+  let chainsWithId: IChainsWithId[] = await getCoinGeckoChains();
   const coinGeckoId = chainsWithId.filter(
     (chain) => chainId === chain.chain_identifier,
   )[0].id;
 
   const tokenAddresses = balances
-    .reduce((arr, token) => {
+    .reduce((arr: string[], token) => {
       arr.push(token.address);
       return arr;
     }, [])
@@ -37,5 +68,6 @@ export const getPriceInformation = async (balances, chainId) => {
 
   // add price information and return values
   const balancesWithValues = updateTokensWithPriceInfo(balances, prices);
+  console.log("balancesWith Values - ", balancesWithValues);
   return balancesWithValues;
 };
