37511304dc4cfda762100c8c0dcea1ad5b523a9e
Michael <michaelksullivan50@gmail.com>
2022-11-08 13:22:54 +0100

    Add compression to webpack file

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,8 @@
 const webpack = require("webpack");
+const CompressionPlugin = require("compression-webpack-plugin");
+const BrotliPlugin = require("brotli-webpack-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = function override(config) {
   const fallback = config.resolve.fallback || {};
@@ -18,5 +22,25 @@ module.exports = function override(config) {
       Buffer: ["buffer", "Buffer"],
     }),
   ]);
+  config.plugins.push(
+    new CompressionPlugin({
+      filename: "[name].gz[query]",
+      algorithm: "gzip",
+      test: /\.(js|css|html|svg)$/,
+      threshold: 8192,
+      minRatio: 0.8,
+    }),
+  );
+
+  config.plugins.push(
+    new BrotliPlugin({
+      //brotli plugin
+      asset: "[path].br[query]",
+      test: /\.(js|css|html|svg)$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+  );
+  config.plugins.push(new BundleAnalyzerPlugin());
   return config;
 };
