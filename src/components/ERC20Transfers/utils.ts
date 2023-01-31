2e2fdafd9969fd3f5063e80c354366aa11a2bdab
nunocgoncalves <nunogoncalves@mercurylabs.xyz>
2023-01-31 17:32:53 +0800

    Edited env.example and added chainId parameter to apiList

--- src/components/ERC20Transfers/utils.ts
+++ src/components/ERC20Transfers/utils.ts
@@ -1,56 +1,85 @@
 import axios from "axios";
 
-export { default } from "./ERC20Transfers";
-
-const address = "0x052D1f06cC98AAe6c4D2e9D06c286143E22dB612";
+// todo: connect to user account
+const address = process.env.REACT_APP_TEST_ACCOUNT;
 
 interface ApiInfo {
   endpoint: string;
   apikey: any;
+  chainId: string;
 }
 
 const apiList: ApiInfo[] = [
   {
-    endpoint: "https://api.polygonscan.com/api",
-    apikey: process.env.REACT_APP_POLY_BLOCKCHAIN_API,
-  },
-  {
     endpoint: "https://api.etherscan.io/api",
-    apikey: process.env.REACT_APP_ETH_BLOCKCHAIN_API,
+    apikey: process.env.REACT_APP_ETH_EXPLORER_API,
+    chainId: "",
   },
   {
-    endpoint: "https://api.bscscan.com/api",
-    apikey: process.env.REACT_APP_BSC_BLOCKCHAIN_API,
+    endpoint: "https://api.polygonscan.com/api",
+    apikey: process.env.REACT_APP_POLY_EXPLORER_API,
+    chainId: "",
   },
   {
     endpoint: "https://api.ftmscan.com/api",
-    apikey: process.env.REACT_APP_FTM_BLOCKCHAIN_API,
-  },
-  {
-    endpoint: "https://api.gnosisscan.io/api",
-    apikey: process.env.REACT_APP_GNO_BLOCKCHAIN_API,
+    apikey: process.env.REACT_APP_FTM_EXPLORER_API,
+    chainId: "",
   },
   {
     endpoint: "https://api.snowtrace.io/api",
-    apikey: process.env.REACT_APP_AVA_BLOCKCHAIN_API,
-  },
-  {
-    endpoint: "https://api.arbiscan.io/api",
-    apikey: process.env.REACT_APP_ARBI_BLOCKCHAIN_API,
+    apikey: process.env.REACT_APP_AVAX_EXPLORER_API,
+    chainId: "",
   },
   {
     endpoint: "https://api-optimistic.etherscan.io/api",
-    apikey: process.env.REACT_APP_OP_BLOCKCHAIN_API,
+    apikey: process.env.REACT_APP_OP_EXPLORER_API,
+    chainId: "",
+  },
+  {
+    endpoint: "https://api.arbiscan.io/api",
+    apikey: process.env.REACT_APP_ARB_EXPLORER_API,
+    chainId: "",
+  },
+  {
+    endpoint: "https://api.bscscan.com/api",
+    apikey: process.env.REACT_APP_BSC_EXPLORER_API,
+    chainId: "",
+  },
+  {
+    endpoint: "https://api.gnosisscan.io/api",
+    apikey: process.env.REACT_APP_GNOSIS_EXPLORER_API,
+    chainId: "",
+  },
+  {
+    endpoint: "https://explorer.celo.org/mainnet/api",
+    apikey: null,
+    chainId: "",
   },
 ];
 
-// ERC 20 Transaction
-export const tokenTransaction = async (api: ApiInfo) => {
+export const transaction = async (api: ApiInfo, transactionType: string) => {
   try {
+    let action;
+    switch (transactionType) {
+      case "token":
+        action = "tokentx";
+        break;
+      case "nft":
+        action = "tokennfttx";
+        break;
+      case "1155":
+        action = "token1155tx";
+        break;
+      case "normal":
+        action = "txlist";
+        break;
+      default:
+        throw new Error("Invalid transaction type");
+    }
     const response = await axios.get(api.endpoint, {
       params: {
         module: "account",
-        action: "tokentx",
+        action,
         offset: 10000,
         sort: "desc",
         address: `${address}`,
@@ -62,88 +91,4 @@ export const tokenTransaction = async (api: ApiInfo) => {
   } catch (error) {
     console.error(error);
   }
-};
-
-export const allTokenTransaction = async () => {
-  const apiPromises = apiList.map((api) => tokenTransaction(api));
-  const dataFromAllApis = await Promise.all(apiPromises);
-  return dataFromAllApis;
-};
-
-// ERC 721 transaction
-export const nftData = async (api: ApiInfo) => {
-  try {
-    const response = await axios.get(api.endpoint, {
-      params: {
-        module: "account",
-        action: "tokennfttx",
-        offset: 10000,
-        sort: "desc",
-        address: `${address}`,
-        apikey: api.apikey,
-      },
-    });
-    const data = response.data.result;
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-export const allNftData = async () => {
-  const apiPromises = apiList.map((api) => nftData(api));
-  const dataFromAllApis = await Promise.all(apiPromises);
-  return dataFromAllApis;
-};
-
-// ERC-1155 Transaction
-export const erc1155Data = async (api: ApiInfo) => {
-  try {
-    const response = await axios.get(api.endpoint, {
-      params: {
-        module: "account",
-        action: "token1155tx",
-        offset: 10000,
-        sort: "desc",
-        address: `${address}`,
-        apikey: api.apikey,
-      },
-    });
-    const data = response.data.result;
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-export const allERC1155Data = async () => {
-  const apiPromises = apiList.map((api) => erc1155Data(api));
-  const dataFromAllApis = await Promise.all(apiPromises);
-  return dataFromAllApis;
-};
-
-// Normal Transaction
-export const normalTransaction = async (api: ApiInfo) => {
-  try {
-    const response = await axios.get(api.endpoint, {
-      params: {
-        module: "account",
-        action: "txlist",
-        offset: 10000,
-        sort: "desc",
-        address: `${address}`,
-        apikey: api.apikey,
-      },
-    });
-    const data = response.data.result;
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-export const allNormalTransaction = async () => {
-  const apiPromises = apiList.map((api) => normalTransaction(api));
-  const dataFromAllApis = await Promise.all(apiPromises);
-  return dataFromAllApis;
 };
