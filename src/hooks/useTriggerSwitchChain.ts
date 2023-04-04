7c3eb0c70a80174a143a0a83012cda7a0b64d68c
Nuno Gonçalves <nunogoncalves@mercurylabs.xyz>
2023-04-04 12:14:32 +0100

    Add starter code for trigger switch chain hook

--- src/hooks/useTriggerSwitchChain.ts
+++ src/hooks/useTriggerSwitchChain.ts
@@ -1,1 +1,11 @@
 // todo
+
+import { useWeb3AuthExecutionStore } from "../stores/web3Auth";
+
+export const useTriggerSwitchChain = () => {
+  const { web3auth } = useWeb3AuthExecutionStore(
+    (state: any) => state,
+  );
+
+  // logic handling
+};
